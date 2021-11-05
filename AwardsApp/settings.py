c4eb0a87ab528c2166d1bd27e3ec6302e7ef08df
NancyWachiuri <nancywachiuri@gmail.com>
2021-11-05 06:25:59 +0300

    import django heroku

--- AwardsApp/settings.py
+++ AwardsApp/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-import 
+import django_heroku
 import dj_database_url
 from decouple import config,Csv
 import cloudinary_storage
