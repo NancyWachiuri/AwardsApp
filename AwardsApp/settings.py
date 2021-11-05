ff39d071e2f614763cb2787d0d1d0259ae7bd925
NancyWachiuri <nancywachiuri@gmail.com>
2021-11-05 05:58:40 +0300

    Add database config to settings

--- AwardsApp/settings.py
+++ AwardsApp/settings.py
@@ -33,7 +33,36 @@ DEBUG = os.environ.get('DEBUG', True)
 # development
 if config('MODE')=="dev":
 
-ALLOWED_HOSTS = []
+
+
+
+    DATABASES = {
+       'default': {
+           'ENGINE': 'django.db.backends.postgresql_psycopg2',
+           'NAME': config('DB_NAME'),
+           'USER': config('DB_USER'),
+           'PASSWORD': config('DB_PASSWORD'),
+           'HOST': config('127.0.0.1'),
+           'PORT': '',
+       }
+   }
+# production
+else:
+   DATABASES = {
+       'default': dj_database_url.config(
+           default=config('DATABASE_URL')
+       )
+   }
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv(),)
+
+
+ALLOWED_HOSTS = ['.localhost','.herokuapp.com','.127.0.0.1']
+
+
+
+
 
 
 # Application definition
